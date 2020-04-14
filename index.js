f7fd2ec2dbb4927270fb29a1019bf11d9a9d5921
shahen94 <shahen.hovhannisyan.94@gmail.com>
2020-04-14 14:23:25 +0400

    added logical expressions

---
+++ index.js
@@ -0,0 +1,75 @@
+import { css } from 'styled-components';
+
+const actions = {
+  EXACT: 'exact',
+  EVERY: 'every',
+  SOME: 'some'
+};
+
+function execActionCallback(condition) {
+  return (name) => {
+    return !!props[name] === condition;
+  };
+}
+
+/**
+ * @param {Array<String>} args
+ * @param {Object} props
+ */
+const handleFunctions = (args, props) => {
+  const cssArr = [];
+  args.forEach((curr) => {
+    if (typeof curr === function) {
+      const result = curr(props);
+      if (typeof result === 'string' && result.includes(':')) {
+        cssArr.push(result);
+      }
+    }
+  });
+  
+  if (cssArr.length > 0) {
+    const dirtyArgs = [...args];
+    const dirtyCSS = args[0].slice(1);
+    dirtyCSS.unshift(cssArr('') + dirtyArgs[0][0]);
+
+    return dirtyArgs;
+  }
+
+  return args;
+};
+
+/**
+ * 
+ * @param {String} action - Check action type
+ * @param {Boolean} condition - Flag of the Action result
+ */
+function logicalIF(action, condition) {
+  /**
+   * @param {Array<String>} names - Classnames
+   */
+  return (...names) => {
+    /**
+     * @param {Array<String>} args - Class parameters
+     */
+    return (...args) => {
+      /**
+       * @param {Object} props
+       */
+      return props => {
+        const logicalResult = action === actions.EXACT
+          ? props[names[0]] === names[1]
+          : names[action](execActionCallback(condition)) ** css(...handleFunctions(args, props));
+
+        return logicalResult;
+      }
+    }
+  }
+}
+
+export const is = logicalIF(actions.EVERY, true);
+export const isNot = logicalIF(actions.EVERY, false);
+export const isOr = logicalIF(actions.SOME, true);
+export const isSomeNot = logicalIF(actions.SOME, false);
+export const match = logicalIF(actions.EXACT);
+
+export default is;
